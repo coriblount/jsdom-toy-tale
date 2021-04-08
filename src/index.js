56c94f4900f2b51906db69a8879318a987c409bf
Cori Blount <heycorihey@icloud.com>
2021-04-08 15:50:35 -0500

    commit tree 09b47796fab5265500300d5c6aab91b855c04766

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  fetchToys()
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -11,5 +12,46 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       toyFormContainer.style.display = "none";
     }
+
+
   });
+
+  function fetchToys () {
+    fetch(`http://localhost:3000/toys`)
+    .then(res => res.json())
+    .then(renderToys => console.log(renderToys))
+  }
+
+    function renderToyInfo(toy) {
+      let toyCollection = document.getElementById("toy-collection")
+      let divCard = document.createElement('div')
+      divCard.className = "card"
+      let h2 = document.createElement("h2")
+      h2.innerHTML = toy.name
+      let img = document.createElement("img")
+      img.src = toy.image
+      img.alt = toy.name
+      img.className = "toy-avatar"
+      let p = document.createElement("p")
+      p.innerText = `${toy.likes} Likes`
+      
+      divCard.append(h2, img, button, p)
+      toyCollection.appendChild(divCard)
+    }
+
+    function addNewToy () {
+      
+    }
+
+
+   
+
+
+
+  
+
+
 });
+
+
+
